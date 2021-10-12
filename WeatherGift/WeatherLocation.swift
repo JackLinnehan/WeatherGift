d163a3a34b94a1c26d74d6c3f305b33fc192f816
JackLinnehan <linnehjb@bc.edu>
2021-10-12 08:50:18 -0400

    Initial Commit

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by John Linnehan on 10/12/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
